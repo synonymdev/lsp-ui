b4e4d030f56ffad99719246d97acb679ddb258f9
Jason van den Berg <jayvdb1@gmail.com>
2022-03-02 16:12:29 +0000

    Claim view mobile layout fixes

--- src/components/heading/index.tsx
+++ src/components/heading/index.tsx
@@ -2,5 +2,5 @@ import React  from 'react';
 import './index.scss';
 
 export default ({ children }: { children: string; }): JSX.Element => {
-	return <div className={'page-heading'}><h1>{children}</h1></div>;
+	return <div className={'page-heading'}><span>{children}</span></div>;
 };
