f45fe3cc334521f7770de59c3e2c813a74097cbf
Jason van den Berg <jayvdb1@gmail.com>
2022-04-20 17:03:09 +0200

    Status copy and icon updates

--- src/pages/public/error/index.tsx
+++ src/pages/public/error/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import FormCard from '../../../components/form-card';
 import Heading from '../../../components/heading';
-import IconRing from '../../../components/icon-ring';
+import IconRing, { TIcon } from '../../../components/icon-ring';
 import SupportLink from '../../../components/support-link';
 
 import './index.scss';
@@ -12,16 +12,20 @@ function ErrorPage(props: {
 }): JSX.Element {
 	let headerMessage = 'An error occurred. Please try again later.';
 	let backPage: TPublicPage | undefined;
+	let icon: TIcon = 'wrench-3d';
+
 	switch (props.type) {
 		case 'geoblocked':
-			headerMessage = 'Blocktank services are not available in your area.';
+			headerMessage = 'Sorry, Blocktank is not (yet) available in your area. ';
+			icon = 'globe-3d';
 			break;
 		case 'orderNotFound':
 			headerMessage = 'This order could not be found.';
 			backPage = 'configure';
 			break;
 		case 'unavailable':
-			headerMessage = 'Service unavailable. Please try again later.';
+			headerMessage = 'Blocktank is currently offline. Please try again later.';
+			icon = 'wrench-3d';
 			break;
 		case 'generic':
 			backPage = 'configure';
@@ -34,7 +38,7 @@ function ErrorPage(props: {
 			<div className={'error-page-container'}>
 				<p className={'error-page-message'}>{headerMessage}</p>
 				<div className={'error-page-content'}>
-					<IconRing icon={'thumb-down-3d'} type={'error'} />
+					<IconRing icon={icon} type={'error'} />
 					<div className={'support-button-container'}>
 						<SupportLink />
 					</div>
